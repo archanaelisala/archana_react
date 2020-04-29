aa835705fe178f91d8fb459d019144d357dd8212
archanaelisala <archanaelisala@gmail.com>
2020-04-29 17:43:17 +0530

    Updates

---
+++ precache-manifest.1d1c5d73adf5100ce15c41dbf89fca33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f7c9272bd182969fb846f65604542b",
+    "url": "/archana_react/index.html"
+  },
+  {
+    "revision": "9ec808c13c6119053fab",
+    "url": "/archana_react/static/css/main.c0d663da.chunk.css"
+  },
+  {
+    "revision": "426d3d45c4e44decaf6f",
+    "url": "/archana_react/static/js/2.90f1508f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/archana_react/static/js/2.90f1508f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec808c13c6119053fab",
+    "url": "/archana_react/static/js/main.46e452af.chunk.js"
+  },
+  {
+    "revision": "838e5ca374d48d4bd614",
+    "url": "/archana_react/static/js/runtime-main.f463826a.js"
+  }
+]);
